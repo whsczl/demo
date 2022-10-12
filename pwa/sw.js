1187444f9ffbffe072d98c32161c9c8c062789d1
whsczl <437868141@qq.com>
2022-10-12 18:18:22 +0800

    Add files via upload

--- pwa/sw.js
+++ pwa/sw.js
@@ -9,24 +9,24 @@ var cacheList=[
   pathname+'/main.css',
   pathname+'/icon.png'
 ]
-self.addEventListener('install',e =>{
-  e.waitUntil(
-    caches.open(cacheStorageKey)
-    .then(cache => cache.addAll(cacheList))
-    .then(() => self.skipWaiting())
-  )
-})
+//self.addEventListener('install',e =>{
+//  e.waitUntil(
+//    caches.open(cacheStorageKey)
+//    .then(cache => cache.addAll(cacheList))
+//    .then(() => self.skipWaiting())
+//  )
+//})
 
-self.addEventListener('fetch',function(e){
-  e.respondWith(
-    caches.match(e.request).then(function(response){
-      if(response != null){
-        return response
-      }
-      return fetch(e.request.url)
-    })
-  )
-})
+//self.addEventListener('fetch',function(e){
+//  e.respondWith(
+//    caches.match(e.request).then(function(response){
+//      if(response != null){
+//        return response
+//      }
+//      return fetch(e.request.url)
+//    })
+//  )
+//})
 self.addEventListener('activate',function(e){
   e.waitUntil(
     //获取所有cache名称
