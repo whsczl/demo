e366b3f4e20ed6ffea4a87039bf5132bdd8fb7fa
whsczl <437868141@qq.com>
2022-10-12 17:04:22 +0800

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,7 +1,6 @@
 //importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
 var cacheStorageKey = 'minimal-pwa-1'
 var cacheList=[
-  '/',
   'index.html',
   'main.css',
   'icon.png'
